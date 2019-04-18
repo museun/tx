2129400a158387c6bf7b75022e9cd3b7c6aa390e
museun <museun@outlook.com>
2019-04-18 04:37:31 -0400

    update readme

--- README.md
+++ README.md
@@ -3,13 +3,20 @@
 ## A simple 'Transaction' style smart pointer
 
 Any changes made to the wrapped type will be reverted if `Tx::commit` wasn't called
-when this is dropped. Changes back be rolled back to the last commit with `Tx::rollback`
+when this is dropped. 
+
+Changes can be rolled back to the last commit with `Tx::rollback`
+
+A trait, `Tx::AsTx` can be imported to implement `Self::tx(&mut self) -> Tx<Self>` on `&mut T`
 
 ## Example
 ```rust
+use tx::Tx;
+
 #[derive(Clone)]
 struct S { d: Vec<u32> }
 impl S {
+    // or use the tx::AsTx trait to provide this automatically
     fn tx(&mut self) -> Tx<'_, Self> {
         Tx::new(self)
     }
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,11 @@
 # A simple 'Transaction' style smart pointer
 
 Any changes made to the wrapped type will be reverted if [`Tx::commit`](./struct.Tx.html#method.commit) wasn't called
-when this is dropped. Changes back be rolled back to the last commit with [`Tx::rollback`](./struct.Tx.html#method.rollback)
+when this is dropped.
+
+Changes can be rolled back to the last commit with [`Tx::rollback`](./struct.Tx.html#method.rollback)
+
+A trait, [`Tx::AsTx`](./trait.AsTx.html) can be imported to implement `Self::tx(&mut self) -> Tx<Self>` on `&mut T`
 
 # Example
 ```
@@ -11,6 +15,7 @@ use tx::Tx;
 #[derive(Clone)]
 struct S { d: Vec<u32> }
 impl S {
+    // or use the tx::AsTx trait to provide this automatically
     fn tx(&mut self) -> Tx<'_, Self> {
         Tx::new(self)
     }
